d13f6494ea6a37e152fbb8c5648895c578255c35
JLSoaresRamos <jlramossoares@outlook.com.br>
2024-01-02 14:26:26 -0300

    Default config for tailwind and prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,18 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'very-dark-gray': 'hsl(0, 0%, 17%)',
+        'dark-gray': 'hsl(0, 0%, 59%)'
+      },
+      fontSize: {
+        input: '1.125rem'
+      },
+      fontFamily: {
+        'rubik': ['Rubik', 'sans-serif'] 
+      },
+    },
   },
   plugins: [],
 }
-
